b6158f8beee34ff8a8db88afb2441422d4a87655
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-06 15:37:46 +0900

    Keras use_multiprocessing False

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -99,7 +99,8 @@ class KerasPilot(object):
                         epochs=epochs, 
                         verbose=1, 
                         validation_data=val_gen,
-                        callbacks=callbacks_list, 
+                        callbacks=callbacks_list,
+                        use_multiprocessing=True, # Add Nakagawa
                         validation_steps=steps*(1.0 - train_split))
         return hist
 
