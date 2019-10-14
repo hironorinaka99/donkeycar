730f9f5d9b6c77d083ad35732fbcf8f3364f0f60
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-14 10:51:00 +0900

    Revert "add print in DistanceSensorMulti run_threded"
    This reverts commit b5c7e03672ff314463ccf51323732f3dd28c2db2.

--- donkeycar/parts/DistanceSensorMulti.py
+++ donkeycar/parts/DistanceSensorMulti.py
@@ -50,7 +50,6 @@ class DistanceSensorMulti():
         return
 
     def run_threaded(self):
-        print("run_thredaed")
         return self.distance1, self.distance2, self.throttle, self.mode
 
     def run(self):
