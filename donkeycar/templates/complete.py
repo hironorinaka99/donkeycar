f0284fd5836e9421a9fe398d962e6029f96645a2
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-20 13:39:26 +0900

    タイマー修正

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -412,8 +412,8 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
             dis_RR_range = 20 #右横センサーの反応範囲 
             dis_RR_rev_range = 8 #右横センサーの後退反応範囲
 
-            dis_timer_all = 1.0 #待ち時間全体
-            dis_timer_back = 0.3 #後退時間
+            dis_timer_all = 1.2 #待ち時間全体
+            dis_timer_back = 0.5 #後退時間
             dis_timer_wait = 0.3 #後退待ち時間
             dis_back_throttle = -0.3 #後退速度
 
