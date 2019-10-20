914b69959da6bfdaadcb3fe26bce774ca8edf2d6
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-20 13:25:32 +0900

    back print

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -466,6 +466,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         #print("set new start time")
                         return pilot_angle, 0 #ニュートラルに戻す
                     elif time_dis_gap > dis_timer_back + dis_timer_wait: #いったんバックする時間
+                        print("back" + str(time_dis_gap))
                         if min(distanceL, distanceC, distanceR) == distanceL and distanceLL > dis_LL_rev_range and distanceRR > dis_RR_rev_range: #左前が近く、横センサーが反応していない条件
                             return -1, dis_back_throttle #左が近い場合は、左にハンドル切って後退
                         elif min(distanceL, distanceC, distanceR) == distanceR and distanceLL > dis_LL_rev_range and distanceRR > dis_RR_rev_range: #右前が近く、横センサーが反応していない条件
@@ -474,6 +475,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                             return 0, dis_back_throttle #中央が近い場合は、ハンドル中央に戻し、後退
 
                     elif time_dis_gap > dis_timer_wait: #バックする為に一度0を入力
+                        print("back wait" + str(time_dis_gap))
                         if min(distanceL, distanceC, distanceR) == distanceL and distanceLL > dis_LL_rev_range and distanceRR > dis_RR_rev_range: #左前が近く、横センサーが反応していない条件:
                             return pilot_angle, 0 #左が近い場合は、スロットル0で待機
                         elif min(distanceL, distanceC, distanceR) == distanceR and distanceLL > dis_LL_rev_range and distanceRR > dis_RR_rev_range: #右前が近く、横センサーが反応していない条件:
