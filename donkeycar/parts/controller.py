bfd26183dfab9ee915bbc77ba29ea86cd06611ab
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-21 16:36:06 +0900

    steering 急ハンドルしない

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -939,12 +939,23 @@ class JoystickController(object):
                 return self.angle, self.throttle, self.mode, self.recording
 
 
-        angle_new = prev_angle + (self.angle - prev_angle) / 2  #ステアリングを鈍感にする
+        """
+        angle_new = prev_angle + (self.angle - prev_angle) / 2  #ステアリング 差分を積算していく
         print("self.angle %.2f" % self.angle +  "    angle_new %.2f" % angle_new  + "   prev_angle  %.2f" % prev_angle)
-
         prev_angle = angle_new #前回ステアリング値をglobal変数で保持
+        """
+        steering_change_val = 0.2 #1ループあたりのステアリング変更値（急ハンドルしない）
+        if abs(self.angle -prev_angle) > steering_change_val: #ステアリングがsteering_change_val以上操作されたら
+            if self.angle - prev_angle > 0:
+                angle_new += steering_change_val  #steering_change_val変化
+            else:
+                angle_new -= steering_change_val #steering_change_val変化
+        else:
+            angle_new = self.angle
 
         return angle_new, self.throttle, self.mode, self.recording
+        #return self.angle, self.throttle, self.mode, self.recording
+
 
 
     def run(self, img_arr=None):
