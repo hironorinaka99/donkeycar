a3248fb56610d7521f809eefdd301eda0c5a0f93
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-21 14:56:01 +0900

    steering print

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -939,6 +939,7 @@ class JoystickController(object):
                 return self.angle, self.throttle, self.mode, self.recording
 
 
+        print("self.angle" + str(self.angle) +  "    calc" + str(prev_steering + (self.angle - prev_steering) / 10)   + "   prev_steering" + str(prev_steering))
         self.angle = prev_steering + (self.angle - prev_steering) / 10  #ステアリングを鈍感にする
         prev_steering = self.angle #前回ステアリング値をglobal変数で保持
 
