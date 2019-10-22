1a66837d48118ceccd98a491fc85034707952162
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-22 14:39:27 +0900

    横センサーの反応レンジ 20-> 21へ

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -404,12 +404,12 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                     pilot_angle, pilot_throttle,distanceLL,distanceL,distanceC,distanceR,distanceRR):
             #print("Drive Mode:" + mode)
             global time_dis_short_start
-            dis_LL_range = 20 #左横センサーの反応範囲
+            dis_LL_range = 21 #左横センサーの反応範囲
             dis_LL_rev_range = 8 #左横センサーの後退反応範囲
             dis_L_range = 20 #左センサーの反応範囲 
             dis_C_range = 40 #中央センサーの反応範囲 
             dis_R_range = 20 #右センサーの反応範囲 
-            dis_RR_range = 20 #右横センサーの反応範囲 
+            dis_RR_range = 21 #右横センサーの反応範囲 
             dis_RR_rev_range = 8 #右横センサーの後退反応範囲
             dis_LLRR_value = 0.06 #横センサーの反応係数
 
