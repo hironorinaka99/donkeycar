6237a75dcc4d83e0e1323c6b3fbe39cb93f9c866
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-24 18:35:09 +0900

    Add print for error check

--- donkeycar/parts/coral.py
+++ donkeycar/parts/coral.py
@@ -50,6 +50,8 @@ class InferenceEngine(BasicEngine):
       raise RuntimeError(
           'Invalid input tensor shape! Expected: [1, height, width, 3]')
     _, height, width, _ = input_tensor_shape
+    print("Nakagawa: Error Check height %s" %height + "img.shape[0] %s" %img.shape[0]) #Nakagawa
+    
     assert(height == img.shape[0])
     assert(width == img.shape[1])
     input_tensor = img.flatten()
