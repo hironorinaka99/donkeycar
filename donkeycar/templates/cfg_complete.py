a9ad57205a2681ce7251f3acb1b3e32a8fa95a22
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-25 19:43:55 +0900

    Launch boost , straight boost

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -220,8 +220,8 @@ PUB_CAMERA_IMAGES = False
 #AI_LAUNCH_KEEP_ENABLED = False      # when False ( default) you will need to hit the AI_LAUNCH_ENABLE_BUTTON for each use. This is safest. When this True, is active on each trip into "local" ai mode.
 
 #When racing, to give the ai a boost, configure these values.
-AI_LAUNCH_DURATION = 0.5            # the ai will output throttle for this many seconds
-AI_LAUNCH_THROTTLE = 0.3            # the ai will output this throttle value
+AI_LAUNCH_DURATION = 0.8            # the ai will output throttle for this many seconds
+AI_LAUNCH_THROTTLE = 0.4            # the ai will output this throttle value
 AI_LAUNCH_ENABLE_BUTTON = 'R2'      # this keypress will enable this boost. It must be enabled before each use to prevent accidental trigger.
 AI_LAUNCH_KEEP_ENABLED = True      # when False ( default) you will need to hit the AI_LAUNCH_ENABLE_BUTTON for each use. This is safest. When this True, is active on each trip into "local" ai mode.
 
