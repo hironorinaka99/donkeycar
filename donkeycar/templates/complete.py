11aa3978dca9aca38ce9ac1f3c6d50b9da7f71ae
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-27 08:40:45 +0900

    ハーフスロットル

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -453,7 +453,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
 
                 #距離センサーのギャップ（縮まり方）でぶつかりそうなときはスロットル０
                 if dis_gapL < 0 or dis_gapC < 0 or dis_gapR <0: #前センサーで障害物（距離センサーが縮まっている）発見
-                    user_throttle = 0
+                    user_throttle *= 0.5 
                     print("前方障害物ありのため、スロットル０")
 
                 """
