00006e24790897ffa7578cfe01a96e8ab1679f6b
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-27 05:06:44 +0900

    print prev

--- donkeycar/parts/DistanceSensorMulti4.py
+++ donkeycar/parts/DistanceSensorMulti4.py
@@ -95,19 +95,18 @@ class DistanceSensorMulti4():
         while self.running:
             self.listenToDistanceSensor()
             #self.listenToDistanceSensor(self.distanceLL, self.distanceL, self.distanceC, self.distanceR, self.distanceRR)
-            print (" 5 Update LL: %.1f cm" % self.distanceLL +"L: %.1f cm" % self.distanceL +"  " "C: %.1f cm" % self.distanceC + "  " "R: %.1f cm" % self.distanceR + "  " "RR: %.1f cm" % self.distanceRR)
+            print ("Update LL: %.1f cm" % self.distanceLL +"L: %.1f cm" % self.distanceL +"  " "C: %.1f cm" % self.distanceC + "  " "R: %.1f cm" % self.distanceR + "  " "RR: %.1f cm" % self.distanceRR)
+            print ("UpdatePrev LL: %.1f cm" % self.prev_distanceLL +"L: %.1f cm" % self.prev_distanceL +"  " "C: %.1f cm" % self.prev_distanceC + "  " "R: %.1f cm" % self.prev_distanceR + "  " "RR: %.1f cm" % self.prev_distanceRR)
 
     def listenToDistanceSensor(self):    
     #def listenToDistanceSensor(self, distanceLL, distanceL, distanceC, distanceR, distanceRR):
         #前回測定値を保持
-        print("1 listenToDS")
         self.prev_distanceLL = self.distanceLL
         #print("self.prev_distanceLL" + str(self.prev_distanceLL) + "self.distanceLL" + str(self.distanceLL))
         self.prev_distanceL = self.distanceL
         self.prev_distanceC = self.distanceC
         self.prev_distanceR = self.distanceR
         self.prev_distanceRR = self.distanceRR
-        print("2 listenToDS")
 
         # set Trigger to HIGH  DistanceSensorLeftLeft
         GPIO.output(pinTriggerLL, True)
