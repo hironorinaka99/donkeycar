7408abc39231e4abea038ca30e66b55d81292fb8
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-26 22:33:59 +0900

    修正

--- donkeycar/parts/DistanceSensorMulti4.py
+++ donkeycar/parts/DistanceSensorMulti4.py
@@ -84,7 +84,7 @@ class DistanceSensorMulti4():
             print ("LL: %.1f cm" % self.distanceLL +"L: %.1f cm" % self.distanceL +"  " "C: %.1f cm" % self.distanceC + "  " "R: %.1f cm" % self.distanceR + "  " "RR: %.1f cm" % self.distanceRR) 
 
         print ("LL: %.1f cm" % self.distanceLL +"L: %.1f cm" % self.distanceL +"  " "C: %.1f cm" % self.distanceC + "  " "R: %.1f cm" % self.distanceR + "  " "RR: %.1f cm" % self.distanceRR)
-        print ("Prev LL: %.1f cm" % prev_distanceLL +"L: %.1f cm" % prev_distanceL +"  " "C: %.1f cm" % prev_distanceC + "  " "R: %.1f cm" % prev_distanceR + "  " "RR: %.1f cm" % prev_distanceRR)
+        print ("Prev LL: %.1f cm" % self.prev_distanceLL +"L: %.1f cm" % self.prev_distanceL +"  " "C: %.1f cm" % self.prev_distanceC + "  " "R: %.1f cm" % self.prev_distanceR + "  " "RR: %.1f cm" % self.prev_distanceRR)
         return self.distanceLL, self.distanceL, self.distanceC, self.distanceR, self.distanceRR, self.prev_distanceLL, self.prev_distanceL, self.prev_distanceC, self.prev_distanceR, self.prev_distanceRR
 
     def run(self):
