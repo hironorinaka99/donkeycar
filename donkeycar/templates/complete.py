260f8e05bb2e2f5856740c1cf625e543185e9efc
hironorinaka99 <hironorinaka99@gmail.com>
2019-10-30 21:15:50 +0900

    前方せんさー条件変更

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -481,21 +481,17 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         else:
                             print("距離が縮まっているため準全開ブーストなし")              
 
-                #条件が悪いときに減速
-                if distanceLL < 9 or distanceL < 30 or distanceC < 40 or distanceR < 30 or distanceRR < 9: #減速走行条件
-                    user_throttle *= 0.8 #減速条件整ったら
-                    print("Slow! 前方障害物近い        0.8")
-
                 #print("front left gap %3.1f cm" % dis_gapL + "front cencer gap %3.1f cm" % dis_gapC + "front right gap %3.1f cm" % dis_gapR)
-
+                if distanceLL < 9 or distanceL < 30 or distanceC < 40 or distanceR < 30 or distanceRR < 9: #減速走行条件
+                    user_throttle *= 0.7 #減速条件整ったら
+                    print("Slow! 前方障害物近い        0.7")
                 #距離センサーのギャップ（縮まり方）でぶつかりそうなときに減速
-                if (distanceL > 60 and dis_gapL < 0 and user_angle < -0.3) or (distanceC > 80 and dis_gapC < 0 and abs(user_angle) < 0.5) or (distanceR > 60 and dis_gapR <0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
+                elif (distanceL < 80 and distanceL > 60 and dis_gapL < 0 and user_angle < -0.3) or (distanceC < 100 and distanceC > 80 and dis_gapC < 0 and abs(user_angle) < 0.5) or (ditanceR < 80 and distanceR > 60 and dis_gapR <0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
                     print("front left gap %3.1f cm" % dis_gapL + "front cencer gap %3.1f cm" % dis_gapC + "front right gap %3.1f cm" % dis_gapR)
                     user_throttle *= 0.8
                     print("2前方障害物ありのため、スロットル0.8")
-
                 #距離センサーのギャップ（縮まり方）でぶつかりそうなときはハーフスロットル
-                if (distanceL > 40 and dis_gapL < 0 and user_angle < -0.3) or (distanceC > 60 and dis_gapC < 0 and abs(user_angle) < 0.5) or (distanceR > 40 and dis_gapR <0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
+                elif (distance L < 60 and distanceL > 40 and dis_gapL < 0 and user_angle < -0.3) or (distanceC < 80 and distanceC > 60 and dis_gapC < 0 and abs(user_angle) < 0.5) or (distanceR < 60 and distanceR > 40 and dis_gapR <0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
                     print("front left gap %3.1f cm" % dis_gapL + "front cencer gap %3.1f cm" % dis_gapC + "front right gap %3.1f cm" % dis_gapR)
                     user_throttle *= 0.5
                     print("3前方障害物ありのため、スロットル0.5")
