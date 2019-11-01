f651ac29f01b2afba357c026335147d11dc2bce7
hironorinaka99 <hironorinaka99@gmail.com>
2019-11-01 17:52:24 +0900

    全開速度減少

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -468,9 +468,9 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         print("全開条件成立")
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             print("ギャップ条件成立")
-                            user_angle *= 1.3 #全開条件整ったら
-                            user_throttle *= 1.3
-                            print("boost 1.3")
+                            user_angle *= 1.2 #全開条件整ったら
+                            user_throttle *= 1.2
+                            print("boost 1.2")
                         else:
                             print("距離が縮まっているため全開ブーストなし")              
 
@@ -591,9 +591,9 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         print("全開条件成立")
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             print("ギャップ条件成立")
-                            pilot_angle *= 1.3 #全開条件整ったら
-                            user_throttle *= 1.3
-                            print("boost 1.1")
+                            pilot_angle *= 1.2 #全開条件整ったら
+                            user_throttle *= 1.2
+                            print("boost 1.2")
                         else:
                             print("距離が縮まっているため全開ブーストなし")              
 
@@ -601,7 +601,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             pilot_angle *= 1.1 #準全開条件整ったら
                             user_throttle *= 1.1
-                            print("boost 1.05")
+                            print("boost 1.1")
                         else:
                             print("距離が縮まっているため準全開ブーストなし")              
 
