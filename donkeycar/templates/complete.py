6f7cfaf064abf510a62fadc4929f2b7f2f797387
hironorinaka99 <hironorinaka99@gmail.com>
2019-11-02 11:31:19 +0900

    Fr範囲拡大

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -498,7 +498,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                     print("急速接近中のため　スロットル０")
                 
                 #距離センサーのギャップ（縮まり方）がそこまで大きくないときは小減速
-                elif (distanceL < 80 and distanceL > 20 and dis_gapL < -1.0 and user_angle < -0.3) or (distanceC < 120 and distanceC > 25 and dis_gapC < -1.0 and abs(user_angle) < 0.5) or (distanceR < 80 and distanceR > 20 and dis_gapR < -1.0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
+                elif (distanceL < 80 and distanceL > 20 and dis_gapL < -1.0 and user_angle < -0.3) or (distanceC < 150 and distanceC > 25 and dis_gapC < -2.0 and abs(user_angle) < 0.5) or (distanceR < 80 and distanceR > 20 and dis_gapR < -1.0 and user_angle > 0.3): #前センサーで障害物（距離センサーが縮まっている）発見
                     print("front left gap %3.1f cm" % dis_gapL + "front cencer gap %3.1f cm" % dis_gapC + "front right gap %3.1f cm" % dis_gapR)
                     user_throttle *= 0.5 #テストで０
                     print("微速接近中のため、　スロットル0.5")
