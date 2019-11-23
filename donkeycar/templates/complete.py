51fa1f5d923be1b7c0585ba23b6b6e4746e36b46
hironorinaka99 <hironorinaka99@gmail.com>
2019-11-23 14:36:10 +0900

    速度上げ

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -587,17 +587,17 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         #print("全開条件成立")
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             #print("ギャップ条件成立")
-                            pilot_angle *= 1.3 #全開条件整ったら
-                            user_throttle *= 1.7
-                            #print("boost 1.5")
+                            pilot_angle *= 1.5 #全開条件整ったら
+                            user_throttle *= 2.0
+                            #print("boost 2.0")
                         #else:
                             #print("距離が縮まっているため全開ブーストなし")              
 
                     else: #準全開条件
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
-                            pilot_angle *= 1.1 #準全開条件整ったら
-                            user_throttle *= 1.3
-                            #print("boost 1.3")
+                            pilot_angle *= 1.2 #準全開条件整ったら
+                            user_throttle *= 1.5
+                            #print("boost 1.5")
                         #else:
                             #print("距離が縮まっているため準全開ブーストなし")              
 
