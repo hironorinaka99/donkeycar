9272d805722633c872bf2e804089a81deea95110
hironorinaka99 <hironorinaka99@gmail.com>
2019-11-23 15:39:21 +0900

    ブースト値アップ

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -588,16 +588,16 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                         #print("全開条件成立")
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             #print("ギャップ条件成立")
-                            pilot_angle *= 1.5 #全開条件整ったら
-                            user_throttle *= 2.0
+                            pilot_angle *= 2.0 #全開条件整ったら
+                            user_throttle *= 2.5
                             #print("boost 1.7")
                         #else:
                             #print("距離が縮まっているため全開ブーストなし")              
 
                     else: #準全開条件
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
-                            pilot_angle *= 1.2 #準全開条件整ったら
-                            user_throttle *= 1.3
+                            pilot_angle *= 1.5 #準全開条件整ったら
+                            user_throttle *= 1.8
                             #print("boost 1.3")
                         #else:
                             #print("距離が縮まっているため準全開ブーストなし")              
