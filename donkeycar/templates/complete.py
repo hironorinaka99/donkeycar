db95112432f761078e3aa46b93d960a9625ace01
hironorinaka99 <hironorinaka99@gmail.com>
2019-11-29 23:24:23 +0900

    全開条件　右前を厳しく

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -582,9 +582,15 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
                 #print ("LL: %.1f cm" % distanceLL +"L: %.1f cm" % distanceL +"  " "C: %.1f cm" % distanceC + "  " "R: %.1f cm" % distanceR + "  " "RR: %.1f cm" % distanceRR) 
                 #print("front left gap %3.1f cm" % dis_gapL + "front cencer gap %3.1f cm" % dis_gapC + "front right gap %3.1f cm" % dis_gapR)
                 #if distanceLL > 12 and distanceL > 50 and distanceC > 80 and distanceR > 50 and distanceRR > 12 and abs(pilot_angle) < 0.5: #順全開条件
-                if distanceLL > 140 and distanceL > 100 and distanceC > 120 and distanceR > 50 and distanceRR > 12 and abs(pilot_angle) < 0.4: #順全開条件
+
+                #Sentan
+                #if distanceLL > 140 and distanceL > 100 and distanceC > 120 and distanceR > 50 and distanceRR > 12 and abs(pilot_angle) < 0.4: #順全開条件
+                if distanceLL > 140 and distanceL > 100 and distanceC > 120 and distanceR > 50 and distanceRR > 80 and abs(pilot_angle) < 0.4: #順全開条件
+
                     #print("準全開条件成立")
-                    if distanceL > 120 and distanceC > 150 and distanceR > 60 and abs(pilot_angle) < 0.3: #全開条件
+                    #Sentan
+                    #if distanceL > 120 and distanceC > 150 and distanceR > 60 and abs(pilot_angle) < 0.3: #全開条件
+                    if distanceL > 120 and distanceC > 150 and distanceR > 100 and abs(pilot_angle) < 0.3: #全開条件
                         #print("全開条件成立")
                         if dis_gapL >= 0 and dis_gapC >= 0 and dis_gapR >=0: #前のセンサー距離がどれも縮まっていない
                             #print("ギャップ条件成立")
