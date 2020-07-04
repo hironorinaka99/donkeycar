acd6d1663af215b2d612d710bcca7111537893d4
hironorinaka99 <hironorinaka99@gmail.com>
2020-07-04 16:14:02 +0900

    speedadjust

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -1009,9 +1009,9 @@ class JoystickController(object):
         '''
         #ステアリング値をExponatial 中央付近で鈍感に（２乗）
         if self.angle > 0:
-            return self.angle **2, self.throttle * self.speedadjust, self.mode, self.recording
+            return self.angle **2, self.throttle * (self.speedadjust + 1), self.mode, self.recording
         else:
-            return (self.angle **2) * -1.0, self.throttle * self.speedadjust, self.mode, self.recording
+            return (self.angle **2) * -1.0, self.throttle * (self.speedadjust +1), self.mode, self.recording
 
         #return self.angle, self.throttle, self.mode, self.recording #元設定
 
