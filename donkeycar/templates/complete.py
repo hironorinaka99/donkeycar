947e378aa50325a47fbb017b0c8758d73a00846b
hironorinaka99 <hironorinaka99@gmail.com>
2020-07-07 19:40:23 +0900

    speedadjust

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -455,7 +455,8 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
             #print("Drive Mode:" + mode)
             global time_dis_short_start
             global time_boost_start
-
+            global speedadjust
+            
             dis_LL_range = 30 #左横センサーの反応範囲
             dis_LL_rev_range = 10 #左横センサーの後退反応範囲
             dis_L_range = 25 #左センサーの反応範囲 
