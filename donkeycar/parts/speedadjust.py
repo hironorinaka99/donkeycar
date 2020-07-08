2613633fe1fbcaed7d74f5e2a31e563a9706757c
hironorinaka99 <hironorinaka99@gmail.com>
2020-07-08 22:55:52 +0900

    one

--- donkeycar/parts/speedadjust.py
+++ donkeycar/parts/speedadjust.py
@@ -8,15 +8,15 @@ class speedadjustclass():
         speedadjust = 2.0
         print("In speedincrease",speedadjust)
         return speedadjust
-
+"""
     def speeddecrease(self):
         global speedadjust
         speedadjust = 0.5
         print("In speeddecrease",speedadjust)
         return speedadjust
-    
+"""    
     def run(self):
-        return None
+        return speedadjust
 
 
 
