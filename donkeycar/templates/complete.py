9a4c6619b4c38d19313a2dc454ff790be0fa62b9
hironorinaka99 <hironorinaka99@gmail.com>
2020-07-08 09:41:51 +0900

    speedadjust

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -495,7 +495,8 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None, camera_type
             dis_gapRR = distanceRR - prev_distanceRR
             if abs(dis_gapRR) < dis_gap_ignor_range_side or abs(dis_gapRR) > dis_gap_ignor_range_fast: dis_gapRR = 0 
 
-            print("Speedadjust %5.2f",speedadjust)
+
+            #print("Speedadjust %5.2f",speedadjust)
 
             if mode == 'user': 
                 """
