5d8cfd5af76fbf9286f2bc62b5a0d5d0342e0a30
hironorinaka99 <hironorinaka99@gmail.com>
2020-07-12 10:31:15 +0900

    DMS負のときに120

--- donkeycar/parts/DistanceSensorMulti4.py
+++ donkeycar/parts/DistanceSensorMulti4.py
@@ -85,8 +85,20 @@ class DistanceSensorMulti4():
         #DMSread +=1
         #print("DMS read %8d" % DMSread + "DMS listen %8d" % DMSlisten)
         if self.distanceLL < 0 or self.distanceL < 0 or self.distanceC < 0 or self.distanceR < 0 or self.distanceRR < 0: #エラー処理　マイナス値はエラー表示
-            print("DMS sensor error!!")
+            print("DMS sensor error!!　120cmで返却")
             print ("LL: %.1f cm" % self.distanceLL +"L: %.1f cm" % self.distanceL +"  " "C: %.1f cm" % self.distanceC + "  " "R: %.1f cm" % self.distanceR + "  " "RR: %.1f cm" % self.distanceRR) 
+            if self.distanceLL < 0:
+                self.distanceLL = 120
+            if self.distanceL < 0:
+                self.distanceL = 120
+            if self.distanceC < 0:
+                self.distanceC = 120
+            if self.distanceR < 0:
+                self.distanceR = 120
+            if self.distanceRR < 0:
+                self.distanceRR = 120
+            
+
 
         #print ("Prev LL: %3.1f cm" % self.prev_distanceLL +"L: %3.1f cm" % self.prev_distanceL +"  " "C: %3.1f cm" % self.prev_distanceC + "  " "R: %3.1f cm" % self.prev_distanceR + "  " "RR: %3.1f cm" % self.prev_distanceRR)
         #print ("     LL: %5.1f cm" % self.distanceLL +"L: %5.1f cm" % self.distanceL +"  " "C: %5.1f cm" % self.distanceC + "  " "R: %5.1f cm" % self.distanceR + "  " "RR: %5.1f cm" % self.distanceRR)
