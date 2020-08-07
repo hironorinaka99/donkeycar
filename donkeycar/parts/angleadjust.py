e17d0730814e740daa4f49404d69c4a02c295b22
hironorinaka99 <hironorinaka99@gmail.com>
2020-08-07 14:14:58 +0900

    s

--- donkeycar/parts/angleadjust.py
+++ donkeycar/parts/angleadjust.py
@@ -4,11 +4,11 @@ class angleadjustclass():
         return
 
     def angleincrease(self):
-        self.angleadjust = round(min(1.3, self.angleadjust + 0.05), 2)
+        self.angleadjust = round(min(1.5, self.angleadjust + 0.05), 2)
         print("In angleincrease",self.angleadjust)
 
     def angledecrease(self):
-        self.angleadjust = round(max(0.7, self.angleadjust - 0.05), 2)
+        self.angleadjust = round(max(0.5, self.angleadjust - 0.05), 2)
         print("In angledecrease",self.angleadjust)
 
     def run(self):
