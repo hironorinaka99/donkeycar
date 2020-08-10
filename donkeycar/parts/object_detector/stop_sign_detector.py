ccacff6e304a3bc36a0537e19a311055535169c7
hironorinaka99 <hironorinaka99@gmail.com>
2020-08-10 17:24:49 +0900

    s

--- donkeycar/parts/object_detector/stop_sign_detector.py
+++ donkeycar/parts/object_detector/stop_sign_detector.py
@@ -118,7 +118,8 @@ class StopSignDetector(object):
         if img_arr is None:
             return throttle, img_arr
 
-        img_arr2 = img_arr[0:60, 40:120] #Nakagawa真ん中上だけ注目 H上:H下, W左:W右
+        #img_arr2 = img_arr[0:60, 40:120] #Nakagawa真ん中上だけ注目 H上:H下, W左:W右
+        img_arr2 = img_arr[10:40, 60:100] #Nakagawa真ん中上だけ注目 H上:H下, W左:W右
         img_arr = cv2.resize(img_arr,(160,120)) #Nakagawa W,H
         # Detect traffic light object
         traffic_light_obj = self.detect_stop_sign(img_arr)
